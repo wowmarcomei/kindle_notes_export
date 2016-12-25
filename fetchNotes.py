6977ed21a64f00260f29c46b9b13d0a33d4d2fd2
wowmarcomei <wowmarcomei@gmail.com>
2016-12-25 23:41:13 +0630

    初步完成筛选基本功能

--- fetchNotes.py
+++ fetchNotes.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 
 path = './My_Clippings_20161224.txt'
 
@@ -15,17 +16,21 @@ def parseFile(path):
 mywords_dict = parseFile(path)
 mywords_dict_len = len(mywords_dict)
 
-def parseWords():
+def parseWords(year,month,day):
     for time in range(1,int(1+mywords_dict_len/5)):
         num_list = mywords_dict[1+(time-1)*5]
-        print(re.findall(r"[\d]+",num_list))
-        print("word/sentence is: {}".format(mywords_dict[1+(time-1)*5+2]))
+        # time_list = re.findall(r"[\d]+",num_list)
+        time_list = re.findall(r'\d{4}年\d{1,2}月\d{1,2}', num_list)
+        # print(time_list)
+        selected_time = re.findall(r'[\d]+', str(time_list))
+        # if int(selected_time[0]) == 2016 and int(selected_time[1]) == 7:
+        #     print("yes,2016 07.")
+        # # print(int(selected_time[0]))
 
-parseWords()
+        if int(selected_time[0]) == year and int(selected_time[1]) == month and int(selected_time[2]) == day:
+            print("{} - {} - {}\n{}".format(year,month,day,mywords_dict[1+(time-1)*5+2]))
 
-# date = mywords_dict[6]
-# print(re.findall(r"[\d]+",date))
-#
-#
-# for i in range(1,6):
-#     print("{} of {} is: {}\n".format(i,len(mywords_dict),mywords_dict[i-1]))
+
+        # print("word/sentence is: {}".format(mywords_dict[1+(time-1)*5+2]))
+
+parseWords(2016,12,22)
