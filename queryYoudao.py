5f5705929cadfe33b0c5b92b365cd2b90c689e87
wowmarcomei <wowmarcomei@gmail.com>
2017-03-12 23:24:56 +0630

    完成有道词典网络查词语整理markdown的基本功能

--- queryYoudao.py
+++ queryYoudao.py
@@ -6,7 +6,7 @@ import sys
 import linecache
 import re
 
-words,sentences = [],[]
+words,explainations,sentences = [],[],[]
 
 def extract_Words_Sentences(input=None,output_words=None,output_sentences=None):
     '''
@@ -31,46 +31,52 @@ def extract_Words_Sentences(input=None,output_words=None,output_sentences=None):
                 output_sentences.append(match.group(0))
     file.close()
 
-def queryYoudao(input=None,output=None):
-    with open(output, 'w', encoding='utf-8') as finalmk:
-        print("到有道词典网站查询单词单词,抓取释义!")
-
-        rooturl = 'http://www.youdao.com/w/'
-
-        for item in range(0,len(input)):
-            url = rooturl + input[item]
-
-            response = requests.get(url)
-            soup = bs4.BeautifulSoup(response.text,'html.parser')
-
-            chn2eng = soup.select('#phrsListTab > div.trans-container > ul > li')
-            eng2eng = soup.select('#tEETrans > div > ul > li > ul > li > span')
-
-            shiyi = ''
-            for i in range(0,len(chn2eng)):
-                # print("shiyi {}: {}".format(i,chn2eng[i].get_text()))
-                shiyi = shiyi+chn2eng[i].get_text()+ "  |  "
-
-            for i in range(0,len(eng2eng)):
-                # print("shiyi {}: {}".format(i,eng2eng[i].get_text()))
-                shiyi = shiyi+eng2eng[i].get_text()+ "  |  "
-            print("============================\n查出{}个中文释义,{}个英文释义\n".format(len(chn2eng),len(eng2eng)))
-            print("单词: {}\n释义: {}\n".format(input[item],shiyi))
-            print("****************************\n")
-
-def output_final_markdown():
+def queryYoudao(input=None):
     '''
-    输出最终的markdown文件
+    查询有道词典,输入单词列表,返回单词解释
+    :param input:
     :return:
     '''
-    # 找出单词所在的句子
-    for i in range(0, len(words)):
-        for j in range(0, len(sentences)):
-            if words[i] in sentences[j]:
-                print("word:'{}' is in the sentence:{}\n".format(words[i], sentences[j]))
+    rooturl = 'http://www.youdao.com/w/'
+
+    for item in range(0,len(input)):
+        url = rooturl + input[item]
+
+        response = requests.get(url)
+        soup = bs4.BeautifulSoup(response.text,'html.parser')
+
+        chn2eng = soup.select('#phrsListTab > div.trans-container > ul > li')
+        eng2eng = soup.select('#tEETrans > div > ul > li > ul > li > span')
+
+        shiyi = ''
+        for i in range(0,len(chn2eng)):
+            # print("shiyi {}: {}".format(i,chn2eng[i].get_text()))
+            shiyi = shiyi+chn2eng[i].get_text()+ "  |  "
+
+        for i in range(0,len(eng2eng)):
+            # print("shiyi {}: {}".format(i,eng2eng[i].get_text()))
+            shiyi = shiyi+eng2eng[i].get_text()+ "  |  "
+        # 将解释的单词放置在列表中
+        explainations.append(shiyi)
+    # print("length is: {}\nexplaination is: {}\n".format(len(explaination),explaination))
 
 extract_Words_Sentences(input='static'+'/'+'words_chapters_mxh.md',output_words=words,output_sentences=sentences)
 
-print("words are: {}\nsentences are: {}".format(words,sentences))
+queryYoudao(input=words)
 
-queryYoudao(input=words,output='static'+'/'+'final.md')
+def output_final_markdown():
+    word_in_sentences = False
+    for i in range(0, len(words)):
+        print("========word[{}]=========\n".format(i+1))
+        for j in range(0, len(sentences)):
+            if words[i] in sentences[j]:
+                print("word: {}\nexplaination: {}\nsentences: {}\n========end[{}]=========\n".format(words[i], explainations[i],sentences[j],i+1))
+                # print("========end[{}]=========\n".format(i + 1))
+            else:
+                word_in_sentences = True
+        if word_in_sentences:
+            word_in_sentences = False
+            print("word: {}\nexplaination: {}\n========end[{}]=========\n".format(words[i], explainations[i],i+1))
+            # print("========end[{}]=========\n".format(i + 1))
+
+output_final_markdown()
