0c8dae155ef616fe1206488d02fd31073d24f579
wowmarcomei <wowmarcomei@gmail.com>
2017-03-15 07:57:49 +0630

    ...

--- queryYoudao.py
+++ queryYoudao.py
@@ -17,6 +17,7 @@ def extract_Words_Sentences(input=None,output_words=None,output_sentences=None):
     :return: NULL
     '''
     file = open(input,'r',encoding='utf-8')
+    print("正在解析单词与语句...\n")
     markdown_str = file.read()
     regex = [r'(?<=\|\*{2}).+?(?=\.?\,?\!?\:?\;?\"?\*{2}\|)',r'((?<=\|)[^\w\|]*(\w+\s+(?=\w+)[^\|]*))']
 
@@ -29,6 +30,7 @@ def extract_Words_Sentences(input=None,output_words=None,output_sentences=None):
             else:
                 # 输出句子
                 output_sentences.append(match.group(0))
+    print("解析单词语句结束...\n")
     file.close()
 
 def queryYoudao(input=None):
@@ -39,6 +41,8 @@ def queryYoudao(input=None):
     '''
     rooturl = 'http://www.youdao.com/w/'
 
+    print("正在查询有道词典...\n")
+
     for item in range(0,len(input)):
         url = rooturl + input[item]
 
@@ -48,35 +52,40 @@ def queryYoudao(input=None):
         chn2eng = soup.select('#phrsListTab > div.trans-container > ul > li')
         eng2eng = soup.select('#tEETrans > div > ul > li > ul > li > span')
 
-        shiyi = ''
+        print("word {}: {}".format(item,input[item]))
+        explaination = ''
         for i in range(0,len(chn2eng)):
-            # print("shiyi {}: {}".format(i,chn2eng[i].get_text()))
-            shiyi = shiyi+chn2eng[i].get_text()+ "  |  "
+            print("explaination {}: {}".format(i,chn2eng[i].get_text()))
+            explaination = explaination+chn2eng[i].get_text()
 
         for i in range(0,len(eng2eng)):
-            # print("shiyi {}: {}".format(i,eng2eng[i].get_text()))
-            shiyi = shiyi+eng2eng[i].get_text()+ "  |  "
+            print("explaination {}: {}".format(i,eng2eng[i].get_text()))
+            explaination = explaination+eng2eng[i].get_text()
+        print("===============================================\n")
         # 将解释的单词放置在列表中
-        explainations.append(shiyi)
-    # print("length is: {}\nexplaination is: {}\n".format(len(explaination),explaination))
+        explainations.append(explaination)
+    print("查询有道词典结束...\n")
+
+def output_final_markdown(final_markdown=None):
+
+    print("正在生成最终markdown文件...\n")
+    with open(final_markdown,'a+',encoding='utf-8') as markdown:
+        markdown.write("| 单词 | 解释 | 例句 |\n| --------- | -------- | --------- |\n")
+        word_in_sentences = False
+
+        for i in range(0, len(words)):
+            print("========word[{}]=========\n".format(i+1))
+            for j in range(0, len(sentences)):
+                if words[i] in sentences[j]:
+                    print("word: {}\nexplaination: {}\nsentences: {}\n========end[{}]=========\n".format(words[i], explainations[i],sentences[j],i+1))
+                    markdown.write("|{}|{}|{}|\n".format(words[i],explainations[i],sentences[j]))
+                else:
+                    word_in_sentences = True
+            if word_in_sentences:
+                word_in_sentences = False
+                print("word: {}\nexplaination: {}\n========end[{}]=========\n".format(words[i], explainations[i],i+1))
+                markdown.write("|{}|{}|- |\n".format(words[i],explainations[i]))
 
 extract_Words_Sentences(input='static'+'/'+'words_chapters_mxh.md',output_words=words,output_sentences=sentences)
-
 queryYoudao(input=words)
-
-def output_final_markdown():
-    word_in_sentences = False
-    for i in range(0, len(words)):
-        print("========word[{}]=========\n".format(i+1))
-        for j in range(0, len(sentences)):
-            if words[i] in sentences[j]:
-                print("word: {}\nexplaination: {}\nsentences: {}\n========end[{}]=========\n".format(words[i], explainations[i],sentences[j],i+1))
-                # print("========end[{}]=========\n".format(i + 1))
-            else:
-                word_in_sentences = True
-        if word_in_sentences:
-            word_in_sentences = False
-            print("word: {}\nexplaination: {}\n========end[{}]=========\n".format(words[i], explainations[i],i+1))
-            # print("========end[{}]=========\n".format(i + 1))
-
-output_final_markdown()
+output_final_markdown(final_markdown='static'+'/'+'final_output.md')
